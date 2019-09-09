efe7abf69af32d7317759232aedfba2329539b13
timrozday <48996806+timrozday@users.noreply.github.com>
2019-09-09 10:54:48 +0100

    Create repository by uploading setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='text_query',
+    url='https://github.com/timrozday/inchi_compare',
+    author='Tim Rozday',
+    author_email='timrozday@ebi.ac.uk',
+    # Needed to actually package something
+    packages=['text_query'],
+    # Needed for dependencies
+    install_requires=[],
+    version='0.1',
+    # The license can be anything you like
+    license='Do what you like with it (just nothing evil)',
+    description='A few functions for parsing XML, creating varients of sentences and querying them with an index.',
+    # We will also need a readme eventually (there will be a warning)
+    long_description='A few functions for parsing XML, creating varients of sentences and querying them with an index.',
+    # long_description=open('README.txt').read(),
+)
