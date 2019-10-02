c55d18db321328a7702ed934fc09c20106f791fc
Tim Rozday <timrozday@ebi.ac.uk>
2019-10-02 16:42:06 +0100

    bugfix after kmer index database was changed

--- text_query/text_query.py
+++ text_query/text_query.py
@@ -368,7 +368,7 @@ def query_index_db(kmer, db_conn):
     kmer_str = re.sub('\'', '\'\'', repr(kmer)) # handle quotes
     results = db_conn.execute(f"select st.onto_id, st.source, st.string, st.expanded_sentences from (select * from (select * from kmers where kmer='{kmer_str}') km left join kmer_to_string on km.id=kmer_to_string.kmer_id) ks inner join (select * from strings) st on ks.string_id=st.id")
     for onto_id, source, string, sentence in results:
-        yield {'source': source, 'onto_id': onto_id, 'string': string, 'sentence': eval(sentence)[0]}
+        yield {'source': source, 'onto_id': onto_id, 'string': string, 'sentence': eval(sentence)}
 
 def kmer_query(sentence, query_f, f_args, stop_words={'of', 'type', 'with', 'and', 'the', 'or', 'due', 'in', 'to', 'by', 'as', 'a', 'an', 'is', 'for', '.', ',', ':', ';', '?', '-', '(', ')', '/', '\\', '\'', '"', '\n', '\t', '\r'}):
     # generate kmers from conn
